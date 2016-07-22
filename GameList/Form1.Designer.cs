3dd0080cde03ab5de7c2e4688520e2f48e83cb48
Zach Christensen <zachgchristensen@gmail.com>
2016-07-23 01:12:24 +1200

    1.0 First Commit All basic functionality finished such as adding games, saving games, resizing window.

---
+++ GameList/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace GameList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_finish = new System.Windows.Forms.Label();
+            this.lbl_planned = new System.Windows.Forms.Label();
+            this.btn_remove = new System.Windows.Forms.Button();
+            this.btn_finished = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.lbl_finishCount = new System.Windows.Forms.Label();
+            this.listBoxP = new System.Windows.Forms.ListBox();
+            this.listBoxF = new System.Windows.Forms.ListBox();
+            this.btn_unfinish = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_finish
+            // 
+            this.lbl_finish.AutoSize = true;
+            this.lbl_finish.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_finish.Location = new System.Drawing.Point(332, 9);
+            this.lbl_finish.Margin = new System.Windows.Forms.Padding(0, 0, 3, 3);
+            this.lbl_finish.Name = "lbl_finish";
+            this.lbl_finish.Size = new System.Drawing.Size(144, 25);
+            this.lbl_finish.TabIndex = 1;
+            this.lbl_finish.Text = "Finished Games";
+            // 
+            // lbl_planned
+            // 
+            this.lbl_planned.AutoSize = true;
+            this.lbl_planned.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_planned.Location = new System.Drawing.Point(9, 9);
+            this.lbl_planned.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.lbl_planned.Name = "lbl_planned";
+            this.lbl_planned.Size = new System.Drawing.Size(143, 25);
+            this.lbl_planned.TabIndex = 5;
+            this.lbl_planned.Text = "Planned Games";
+            // 
+            // btn_remove
+            // 
+            this.btn_remove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_remove.Location = new System.Drawing.Point(174, 566);
+            this.btn_remove.Name = "btn_remove";
+            this.btn_remove.Size = new System.Drawing.Size(75, 23);
+            this.btn_remove.TabIndex = 5;
+            this.btn_remove.Text = "Remove";
+            this.btn_remove.UseVisualStyleBackColor = true;
+            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click);
+            // 
+            // btn_finished
+            // 
+            this.btn_finished.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_finished.Location = new System.Drawing.Point(93, 566);
+            this.btn_finished.Name = "btn_finished";
+            this.btn_finished.Size = new System.Drawing.Size(75, 23);
+            this.btn_finished.TabIndex = 4;
+            this.btn_finished.Text = "Finished";
+            this.btn_finished.UseVisualStyleBackColor = true;
+            this.btn_finished.Click += new System.EventHandler(this.btn_finished_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_add.Location = new System.Drawing.Point(12, 566);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 3;
+            this.btn_add.Text = "Add Game";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // lbl_finishCount
+            // 
+            this.lbl_finishCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_finishCount.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_finishCount.Location = new System.Drawing.Point(450, 566);
+            this.lbl_finishCount.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.lbl_finishCount.Name = "lbl_finishCount";
+            this.lbl_finishCount.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lbl_finishCount.Size = new System.Drawing.Size(202, 23);
+            this.lbl_finishCount.TabIndex = 9;
+            this.lbl_finishCount.Text = "-/- Games Completed";
+            this.lbl_finishCount.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // listBoxP
+            // 
+            this.listBoxP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxP.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxP.FormattingEnabled = true;
+            this.listBoxP.ItemHeight = 17;
+            this.listBoxP.Location = new System.Drawing.Point(12, 40);
+            this.listBoxP.Name = "listBoxP";
+            this.listBoxP.Size = new System.Drawing.Size(318, 514);
+            this.listBoxP.TabIndex = 1;
+            // 
+            // listBoxF
+            // 
+            this.listBoxF.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxF.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxF.FormattingEnabled = true;
+            this.listBoxF.ItemHeight = 17;
+            this.listBoxF.Location = new System.Drawing.Point(336, 40);
+            this.listBoxF.Name = "listBoxF";
+            this.listBoxF.Size = new System.Drawing.Size(316, 497);
+            this.listBoxF.TabIndex = 2;
+            // 
+            // btn_unfinish
+            // 
+            this.btn_unfinish.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_unfinish.Location = new System.Drawing.Point(336, 566);
+            this.btn_unfinish.Name = "btn_unfinish";
+            this.btn_unfinish.Size = new System.Drawing.Size(75, 23);
+            this.btn_unfinish.TabIndex = 6;
+            this.btn_unfinish.Text = "Unfinish";
+            this.btn_unfinish.UseVisualStyleBackColor = true;
+            this.btn_unfinish.Click += new System.EventHandler(this.btn_unfinish_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 601);
+            this.Controls.Add(this.btn_unfinish);
+            this.Controls.Add(this.listBoxF);
+            this.Controls.Add(this.listBoxP);
+            this.Controls.Add(this.lbl_finishCount);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btn_finished);
+            this.Controls.Add(this.btn_remove);
+            this.Controls.Add(this.lbl_planned);
+            this.Controls.Add(this.lbl_finish);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(600, 300);
+            this.Name = "Form1";
+            this.Text = "Game Tracker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.LoadFromFile);
+            this.Resize += new System.EventHandler(this.Form1_ResizeEnd);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_finish;
+        private System.Windows.Forms.Label lbl_planned;
+        private System.Windows.Forms.Button btn_remove;
+        private System.Windows.Forms.Button btn_finished;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Label lbl_finishCount;
+        private System.Windows.Forms.ListBox listBoxP;
+        private System.Windows.Forms.ListBox listBoxF;
+        private System.Windows.Forms.Button btn_unfinish;
+    }
+}
+
---
+++ GameList/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameList
+{
+    public partial class Form1 : Form
+    {
+        public const char Separator = ((char)007);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            using (var form = new AddGame())
+            {
+                var result = form.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    if (form.gameText != null)
+                    {
+                        listBoxP.Items.Add(form.gameText);
+                    }
+                }
+            }
+            updateGameCount();
+        }
+
+        public void updateGameCount()
+        {
+            var num = listBoxP.Items.Count + listBoxF.Items.Count;
+            lbl_finishCount.Text = listBoxF.Items.Count.ToString() + "/" + num.ToString() + " Games Completed";
+        }
+
+        private void btn_finished_Click(object sender, EventArgs e)
+        {
+            if (listBoxP.SelectedIndex == -1)
+            {
+                MessageBox.Show("To finish a game select it then press the \"Finished\" button.");
+                return;
+            }
+            listBoxF.Items.Add(listBoxP.Items[listBoxP.SelectedIndex]);
+            listBoxP.Items.RemoveAt(listBoxP.SelectedIndex);
+            updateGameCount();
+        }
+
+        private void btn_remove_Click(object sender, EventArgs e)
+        {
+            if (listBoxP.SelectedIndex == -1)
+            {
+                MessageBox.Show("To remove a game select it then press the \"Remove\" button.");
+                return;
+            }
+            listBoxP.Items.RemoveAt(listBoxP.SelectedIndex);
+            updateGameCount();
+        }
+
+        private void btn_unfinish_Click(object sender, EventArgs e)
+        {
+            if (listBoxF.SelectedIndex == -1)
+            {
+                MessageBox.Show("To undo the finishing of a game select it then press the \"Unfinish\" button.");
+                return;
+            }
+            listBoxP.Items.Add(listBoxF.Items[listBoxF.SelectedIndex]);
+            listBoxF.Items.RemoveAt(listBoxF.SelectedIndex);
+            updateGameCount();
+        }
+
+        private void SaveToFile()
+        {
+            using (StreamWriter streamWriter = new StreamWriter("GameList.txt"))
+            {
+                string output = "";
+                int count = 0;
+                foreach (var i in listBoxP.Items)
+                {
+                    if (count != listBoxP.Items.Count - 1)
+                    {
+                        output += i.ToString() + Separator;
+                    }
+                    else
+                    {
+                        output += i.ToString();
+                    }
+                    count += 1;
+                }
+                output += "\0";
+                count = 0;
+                foreach (var i in listBoxF.Items)
+                {
+                    if (count != listBoxF.Items.Count - 1)
+                    {
+                        output += i.ToString() + Separator;
+                    }
+                    else
+                    {
+                        output += i.ToString();
+                    }
+                    count += 1;
+                }
+                output += "\0";
+                output += this.Width;
+                output += Separator;
+                output += this.Height;
+                streamWriter.WriteLine(output);
+            }
+        }
+
+        private void LoadFromFile(object sender, EventArgs e)
+        {
+            using (StreamReader streamReader = new StreamReader("GameList.txt"))
+            {
+                //calc
+                var input = streamReader.ReadLine();
+                string[] Array1 = input.Split('\0');
+                string[] ArrayP = Array1[0].Split(Separator);
+                string[] ArrayF = Array1[1].Split(Separator);
+                string[] ArraySize = Array1[2].Split(Separator);
+                var ArrayP2 = ArrayP.ToList();
+                ArrayP2.Sort();
+                var ArrayF2 = ArrayF.ToList();
+                ArrayF2.Sort();
+                //clear
+                listBoxP.Items.Clear();
+                listBoxF.Items.Clear();
+
+                // write new
+                foreach (var i in ArrayP2)
+                {
+                    listBoxP.Items.Add(i);
+                }
+                foreach (var i in ArrayF2)
+                {
+                    listBoxF.Items.Add(i);
+                }
+
+                //Read old size settings
+                this.Width = int.Parse( ArraySize[0] );
+                this.Height = int.Parse(ArraySize[1]);
+                Form1_ResizeEnd(sender, e);
+
+            }
+            updateGameCount();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveToFile();
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            //width and x positioning
+            int fullWidth = this.Width;
+            int boxWidth = (this.Width - 47)/2;
+            listBoxP.Width = boxWidth;
+            listBoxF.Width = boxWidth;
+            listBoxF.Left = 20 + listBoxP.Width;
+            lbl_finish.Left = listBoxF.Left;
+            btn_unfinish.Left = listBoxF.Left;
+            //Height of boxes
+            int boxHeight = this.Height - 115;
+            listBoxP.Height = boxHeight;
+            listBoxF.Height = boxHeight;
+        }
+    }
+}
